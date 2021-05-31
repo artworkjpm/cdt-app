1fa21bf537c8c1dfa247aa55b12357c3bc81144a
JohnMoran <johnnymoran44@hotmail.com>
2021-05-31 14:17:28 +0200

    active item left-side now changes color for active route

--- src/components/LeftSideBar/LeftSideBar.tsx
+++ src/components/LeftSideBar/LeftSideBar.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 import "./left-side.scss";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 export default function LeftSideBar() {
 	return (
 		<div>
 			<div className="left-bar">
-				<Link to="/designs">
+				<NavLink to="/designs">
 					<div className="item">Designs</div>
-				</Link>
-				<Link to="/setouts">
+				</NavLink>
+				<NavLink to="/setouts">
 					<div className="item">Setouts</div>
-				</Link>
+				</NavLink>
 			</div>
 		</div>
 	);
