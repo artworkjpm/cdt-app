ceaa63660eb39faff5529c088b863108bfac8279
JohnMoran <johnnymoran44@hotmail.com>
2021-06-02 13:32:08 +0200

    set up enzyme for react 17

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
+
 import App from "./App";
+import { shallow, mount } from "enzyme";
 
 test("renders learn react link", () => {
+	const wrapper = shallow(<App />);
+
+	console.log(wrapper.debug());
 	expect(true).toBe(true);
 });
